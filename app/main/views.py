e79e42d5e8e3641c7ae994a917677b7317b6cc1d
guoda <guo.da@byd.com>
2020-01-16 14:13:42 +0800

    修改密码、修改邮箱和重置密码

--- app/main/views.py
+++ app/main/views.py
@@ -5,3 +5,7 @@ from flask import render_template
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/chart')
+def showChart():
+    return render_template('chart.html')
